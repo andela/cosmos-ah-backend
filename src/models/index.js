00b6db361b4e8620f786a711d0f65407edd01aa3
chikeozulumba <o.devcode@gmail.com>
2019-03-31 18:34:13 +0100

    164964758-task(sequelize): implement all models and migrations
    - deleted test migration file
    - modify config for development db host

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,17 @@
-import user from './user';
+import User from './user';
+import Article from './article';
+import Comment from './comment';
+
+// Define Model Relationships
+
+Article.belongsTo(User); // Multiple Articles belongs to a User
+Article.hasMany(Comment); // An Article can have multiple Comments
+
+Comment.belongsTo(Article); // Multiple Comments belongs to a User
 
 export default {
-  user,
+  // Export Models
+  User,
+  Article,
+  Comment,
 };
