0b925265de9d92656b8986d08bc710b5a2dd765e
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2019-04-03 15:32:38 +0100

    164796882-task(testing) refactor test - refactor test
    [finishes #164796882]

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,37 +1,24 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import app from '../src/index';
+import { startServer } from '../src/server';
 
 const { expect } = chai;
 
 chai.use(chaiHttp);
 
 describe('Root directory Test', () => {
+  let app = null;
+  let agent = null;
+
+  before(async () => {
+    app = await startServer(5000);
+    agent = chai.request(app);
+  });
+
   it('Should return status: 200', (done) => {
-    chai
-      .request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-  it('Should return message', (done) => {
-    chai
-      .request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res.body.message).to.equal('Welcome to the test route');
-        done();
-      });
-  });
-  it('Should return object', (done) => {
-    chai
-      .request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res.body).to.be.an('object');
-        done();
-      });
+    agent.get('/api/v1').end((err, res) => {
+      expect(res).to.have.status(200);
+      done();
+    });
   });
 });
