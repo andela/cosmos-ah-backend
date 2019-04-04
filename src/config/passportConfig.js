19afbf61b2c9e5cae5a6ad371fa8769ee512039f
oluwaseyi000 <adebajo.oluwaseyi@gmail.com>
2019-04-04 10:44:19 +0100

    #164796889 adjust credentials
    using should be able to use twitter for authentication
    
    [finishes $164796889]

--- src/config/passportConfig.js
+++ src/config/passportConfig.js
@@ -3,27 +3,15 @@ import { Strategy as LinkedinStrategy } from 'passport-linkedin-oauth2';
 import userController from '../controllers/authentication/social';
 
 const twitterConfig = {
-  consumerKey: 'Y7SLg4Y0eR6n3203QcpGWAVlB',
-  consumerSecret: 'ua3XAGiBZinEX8toPlfOB2XNqL1MXf8VYzZapirjB6364p4K3r',
+  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+  consumerSecret: process.env.WITTER_CONSUMER_SECRET,
   callbackURL: 'http://localhost:4000/auth/twitter/callback',
   includeEmail: true,
 };
-// const twitterConfig = {
-//   consumerKey: process.env.TWITTER_CONSUMER_KEY,
-//   consumerSecret: process.env.WITTER_CONSUMER_SECRET,
-//   callbackURL: 'http://localhost:4000/auth/twitter/callback',
-//   includeEmail: true,
-// };
 
-// const linkedinConfig = {
-//   clientID: process.env.LINKEDIN_CLIENTID,
-//   clientSecret: process.env.LINKEDIN_CLIENTSERVICE,
-//   callbackURL: 'http://localhost:4000/auth/linkedin/callback',
-//   scope: ['r_emailaddress', 'r_basicprofile'],
-// };
 const linkedinConfig = {
-  clientID: '77vhpd3l5lnv4z',
-  clientSecret: 'Gl9FuRA0xpZpYf7u',
+  clientID: process.env.LINKEDIN_CLIENTID,
+  clientSecret: process.env.LINKEDIN_CLIENTSERVICE,
   callbackURL: 'http://localhost:4000/auth/linkedin/callback',
   scope: ['r_emailaddress', 'r_basicprofile'],
   state: true,
