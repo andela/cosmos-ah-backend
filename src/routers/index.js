eaa8e6c47d0dad1ce1de9adae8967d8f70b8d135
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2019-04-09 11:05:30 +0100

    164892850-feature(auth) refactor to add redirect
    -refactor to add redirect in case of failure
    
    [delivers  #164892850]

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,12 +2,11 @@ import { Router } from 'express';
 import passport from 'passport';
 import { AddArticles, UpdateArticle, DeleteArticle } from '../controllers/article';
 
-
 const router = Router();
 
 router.get('/', (req, res) => res.status(200).json({
   message: 'Welcome to the Authors Haven API',
-}));
+}),);
 
 /**
  * Resource handling articles
@@ -30,11 +29,13 @@ router
 // Route for facebook Authentication
 router.get(
   '/auth/facebook',
-  passport.authenticate('facebook', { authType: 'rerequest', scope: ['email'] }),
+  passport.authenticate(
+    'facebook', { authType: 'rerequest', scope: ['email'] },
+  ),
 );
 router.get(
   '/auth/facebook/callback',
-  passport.authenticate('facebook', { session: false }),
+  passport.authenticate('facebook', { failureRedirect: '/api/v1/auth/login' }),
   (req, res) => {
     res.redirect('/api/v1');
   },
@@ -49,6 +50,5 @@ router.get(
     res.redirect('/api/v1');
   },
 );
-
 
 export default router;
