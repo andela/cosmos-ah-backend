568a1ba4ccd037398c2964b088a3fb33a8c86b23
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2019-04-09 14:45:48 +0100

    164892850-feature(auth) refactor
    -refactor
    
    [delivers  #164892850]

--- src/middlewares/socialCallback.js
+++ src/middlewares/socialCallback.js
@@ -1,7 +1,4 @@
-import bcrypt from 'bcryptjs';
-import db from '../models';
-
-const { User } = db;
+import { User } from '../models';
 
 
 const strategyCallback = async (accessToken, refreshToken, profile, done) => {
@@ -9,13 +6,11 @@ const strategyCallback = async (accessToken, refreshToken, profile, done) => {
     id, displayName, photos, emails
   } = profile;
 
-  const salt = bcrypt.genSaltSync(10);
-  const hashPassword = bcrypt.hashSync(id, salt);
   const [user] = await User.findOrCreate({
     where: { email: emails[0].value },
     defaults: {
       full_name: displayName,
-      password: hashPassword,
+      password: id,
       email: emails[0].value,
       username: emails[0].value,
       image_url: photos[0].value,
