ec808c6219e6368972403c6e481d134ed93ae2a8
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2019-04-09 11:16:01 +0100

    164892850-feature(auth) refactor
    -refactor
    
    [delivers  #164892850]

--- src/routers/index.js
+++ src/routers/index.js
@@ -27,23 +27,18 @@ router
   .patch(UpdateArticle);
 
 // Route for facebook Authentication
-router.get(
-  '/auth/facebook',
-  passport.authenticate(
-    'facebook', { authType: 'rerequest', scope: ['email'] },
-  ),
-);
+router.get('/auth/facebook',
+  passport.authenticate('facebook', { authType: 'rerequest', scope: ['email'] }),);
 
-router.get(
-  '/auth/facebook/callback',
+router.get('/auth/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/api/v1/auth/login' }),
   (req, res) => {
     res.redirect('/api/v1');
-  },
-);
+  },);
 
 // Route for google Authentication
-router.get('/auth/google', passport.authenticate('google', { scope: ['email profile'] }));
+router.get('/auth/google',
+  passport.authenticate('google', { scope: ['email profile'] }));
 
 router.get(
   '/auth/google/callback',
