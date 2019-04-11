1d910f862ffecb86e1cd8c79d33adf0dd391714f
oluwaseyi000 <adebajo.oluwaseyi@gmail.com>
2019-04-11 12:49:10 +0100

    #164796889 add username to token
    add username to authentication token
    
    [finishes #164796889]

--- src/controllers/authentication/socialRedirect.js
+++ src/controllers/authentication/socialRedirect.js
@@ -1,9 +1,9 @@
 import Authenticator from '../../middlewares/authenticator';
 
 const socialRedirect = (req, res) => {
-  const { id, email, role } = req.user;
+  const { id, email, role, username } = req.user;
 
-  const token = Authenticator.generateToken({ id, email, role });
+  const token = Authenticator.generateToken({ id, email, role, username });
   res.cookie('jwt-token', token);
   res.redirect('/api/v1');
 };
