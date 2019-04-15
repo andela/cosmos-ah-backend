35a987dca675eb072cfdaab7a3a741ad68793486
Adebajo Oluwaseyi Omoniyi <adebajo.oluwaseyi@gmail.com>
2019-04-15 11:13:26 +0100

    164796889-linkedin-authentication (#54)
    * 164796889-linkedin-authentication
    
    users can authenticate using linkedin
    
    [finishes 164796889]
    
    * #164796889 connecting to linkedin API
    
    makes request to linkedin API to request for authenticated users data
    
    [finishes #164796889]
    
    * #164796889 generate token for linkedin user
    
    generate user token for linkedin authenticated user
    
    [finished #164796889]
    
    * #164796889 adjusting error response

--- src/controllers/authentication/user.js
+++ src/controllers/authentication/user.js
@@ -1,3 +1,4 @@
+import fetch from 'node-fetch';
 import { User } from '../../models';
 import Auth from '../../middlewares/authenticator';
 import { responseFormat, errorResponseFormat } from '../../utils/index';
@@ -53,6 +54,64 @@ export const createUser = async (req, res) => {
       }
     }
 
+    return res.status(500).json(errorResponseFormat({
+      message: 'Something Went Wrong',
+    }));
+  }
+};
+
+export const linkedinUser = (req, res) => {
+  // redirecto to linkedin page to initiate authourization process and get authourization code
+  res.redirect(`https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.LINKEDIN_CLIENTID}&redirect_uri=${process.env.BASE_URL}/auth/linkedin/callback&state=fooobar&scope=r_liteprofile%20r_emailaddress%20w_member_social`);
+};
+
+export const linkedinCallback = async (req, res) => {
+  try {
+    // get linkedin user authourization token
+    const linkedinToken = await fetch(`https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.BASE_URL}/auth/linkedin/callback&client_id=${process.env.LINKEDIN_CLIENTID}&client_secret=${process.env.LINKEDIN_CLIENTSERVICE}`);
+    const linkedinTokenResult = await linkedinToken.json();
+
+    /**  get user profile information ( id, firstname, lastname, profile picture)
+     with user authourization token */
+    const linkedinUserProfile = await fetch('https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))', {
+      headers: {
+        Authorization: `Bearer ${linkedinTokenResult.access_token}` }
+    });
+    const linkedinUserProfileResult = await linkedinUserProfile.json();
+
+    const firstname = linkedinUserProfileResult.firstName.localized.en_US;
+    const lastname = linkedinUserProfileResult.lastName.localized.en_US;
+    const LinkedinImageUrl = linkedinUserProfileResult.profilePicture['displayImage~'].elements[0].identifiers[0].identifier;
+    const linkedinId = linkedinUserProfileResult.id;
+
+    // get user email information with user authourization token
+    const linkedinUserEmail = await fetch('https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))', {
+      headers: {
+        Authorization: `Bearer ${linkedinTokenResult.access_token}` }
+    });
+
+    const linkedinUserEmailResult = await linkedinUserEmail.json();
+    const linkedinEmail = linkedinUserEmailResult.elements[0]['handle~'].emailAddress;
+
+    // signup or signin linkedin authorize user
+    const user = await User.findOrCreate({
+      where: { email: linkedinEmail },
+      defaults: {
+        fullName: `${lastname} ${firstname}`,
+        email: linkedinEmail,
+        username: `${lastname} ${firstname}`,
+        imageUrl: LinkedinImageUrl,
+        password: linkedinId
+      }
+    });
+
+    const { id, fullName, email, username, imageUrl } = user[0];
+    const token = Auth.generateToken({ id, fullName, email, username, imageUrl });
+
+    res.cookie('jwt-token', token);
+
+    res.redirect('/api/v1');
+  } catch (error) {
     return res.status(500).json(errorResponseFormat({
       message: 'Something Went Wrong',
     }));
