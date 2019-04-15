4cff514903eebc40d93f1fc058ddab4b6f79f8eb
oluwaseyi000 <adebajo.oluwaseyi@gmail.com>
2019-04-15 10:48:05 +0100

    #164796889 generate token for linkedin user
    generate user token for linkedin authenticated user
    
    [finished #164796889]

--- src/controllers/authentication/user.js
+++ src/controllers/authentication/user.js
@@ -61,14 +61,18 @@ export const createUser = async (req, res) => {
 };
 
 export const linkedinUser = (req, res) => {
-  res.redirect('https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=77vhpd3l5lnv4z&redirect_uri=http://localhost:4000/api/v1/auth/linkedin/callback&state=fooobar&scope=r_liteprofile%20r_emailaddress%20w_member_social');
+  // redirecto to linkedin page to initiate authourization process and get authourization code
+  res.redirect(`https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.LINKEDIN_CLIENTID}&redirect_uri=${process.env.BASE_URL}/auth/linkedin/callback&state=fooobar&scope=r_liteprofile%20r_emailaddress%20w_member_social`);
 };
 
 export const linkedinCallback = async (req, res) => {
   try {
-    const linkedinToken = await fetch(`https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://localhost:4000/api/v1/auth/linkedin/callback&client_id=77vhpd3l5lnv4z&client_secret=Gl9FuRA0xpZpYf7u`);
+    // get linkedin user authourization token
+    const linkedinToken = await fetch(`https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.BASE_URL}/auth/linkedin/callback&client_id=${process.env.LINKEDIN_CLIENTID}&client_secret=${process.env.LINKEDIN_CLIENTSERVICE}`);
     const linkedinTokenResult = await linkedinToken.json();
 
+    /**  get user profile information ( id, firstname, lastname, profile picture)
+     with user authourization token */
     const linkedinUserProfile = await fetch('https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))', {
       headers: {
         Authorization: `Bearer ${linkedinTokenResult.access_token}` }
@@ -80,6 +84,7 @@ export const linkedinCallback = async (req, res) => {
     const LinkedinImageUrl = linkedinUserProfileResult.profilePicture['displayImage~'].elements[0].identifiers[0].identifier;
     const linkedinId = linkedinUserProfileResult.id;
 
+    // get user email information with user authourization token
     const linkedinUserEmail = await fetch('https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))', {
       headers: {
         Authorization: `Bearer ${linkedinTokenResult.access_token}` }
@@ -88,7 +93,7 @@ export const linkedinCallback = async (req, res) => {
     const linkedinUserEmailResult = await linkedinUserEmail.json();
     const linkedinEmail = linkedinUserEmailResult.elements[0]['handle~'].emailAddress;
 
-
+    // signup or signin linkedin authorize user
     const user = await User.findOrCreate({
       where: { email: linkedinEmail },
       defaults: {
@@ -100,13 +105,11 @@ export const linkedinCallback = async (req, res) => {
       }
     });
 
-
-    const { id, fullName, email, username, imageUrl } = User.dataValues;
-    console.log(user.User);
-
+    const { id, fullName, email, username, imageUrl } = user[0];
     const token = Auth.generateToken({ id, fullName, email, username, imageUrl });
 
     res.cookie('jwt-token', token);
+
     res.redirect('/api/v1');
   } catch (error) {
     console.log(error);
