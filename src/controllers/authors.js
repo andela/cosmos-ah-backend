4b8d7d868f39b61e96c6209a97e4d0e37e36f4cb
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2019-04-15 09:23:50 +0100

    164796900-feature(endpoint) refactor
    -refactor
    
    [Delivers #164796900]

--- src/controllers/authors.js
+++ src/controllers/authors.js
@@ -1,10 +1,20 @@
 import { User } from '../models';
 
-const getAuthors = (req, res) => {
-  User.findAll({
-    where: { role: 'author' },
-  }).then((authors) => {
-    res.status(200).json({ status: 'success', data: authors });
-  });
+const getAuthors = async (req, res) => {
+  try {
+    const authors = User.findAll({
+      where: { role: 'author' },
+    });
+
+    if (authors) {
+      res.status(200).json({ status: 'success', data: authors });
+    }
+
+    if (authors.length === 0) {
+      res.status(200).json({ status: 'success', message: 'The is no Author on the database' });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 export default getAuthors;
