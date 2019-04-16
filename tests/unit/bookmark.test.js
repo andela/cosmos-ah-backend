b8e75fd23fe8c36a24f24e92b03b79a2662d79da
oluwaseyi000 <adebajo.oluwaseyi@gmail.com>
2019-04-16 11:25:46 +0100

    #164796904 adjusting test
    [finishes  164796904]

--- tests/unit/bookmark.test.js
+++ tests/unit/bookmark.test.js
@@ -10,13 +10,6 @@ describe('validateBookmark()', () => {
       expect(res.passes()).to.be.equal(true);
     });
   });
-  it('should return false if the validation for missing user id field', () => {
-    delete bookmark.userId;
-    const validate = validateBookmark(bookmark);
-    validate.then((res) => {
-      assert.isTrue(res.fails());
-    });
-  });
   it('should return false if the validation for missing article id field', () => {
     delete bookmark.articleId;
     const validate = validateBookmark(bookmark);
