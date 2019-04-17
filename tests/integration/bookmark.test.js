810551306899971b6a59b2bab290ea7f88cf6e24
oluwaseyi000 <adebajo.oluwaseyi@gmail.com>
2019-04-17 09:17:19 +0100

    #164796904 improving test coverage
    improving test coverage
    
    [finishes #164796904]

--- tests/integration/bookmark.test.js
+++ tests/integration/bookmark.test.js
@@ -2,7 +2,7 @@ import 'chai/register-should';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import { startServer } from '../../src/server';
-import { JWT_TOKEN, articleId, invalidArticleUUID } from '../mock/bookmark';
+import { JWT_TOKEN, articleId, invalidArticleId, invalidArticleUUID } from '../mock/bookmark';
 
 
 const { assert } = chai;
@@ -39,6 +39,18 @@ describe('POST /api/v1/:articleId/bookmark', () => {
         assert.isObject(body);
         assert.equal(body.status, 'success');
         assert.equal(body.message, 'Your article has been unbookmarked');
+        done();
+      });
+  });
+
+  it('non-existing article id', (done) => {
+    agent.get(`/api/v1/article/${invalidArticleId}/bookmark`)
+      .set('Authorization', JWT_TOKEN)
+      .end((_err, res) => {
+        assert.equal(res.status, 409);
+        const { body } = res;
+        assert.isObject(body);
+        assert.equal(body.message, 'invalid article id');
         done();
       });
   });
