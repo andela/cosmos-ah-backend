78e90a1583f0354ba18d2c43a280e5b809b9786a
oluwaseyi000 <adebajo.oluwaseyi@gmail.com>
2019-04-18 11:20:53 +0100

    165441966 catch invalid endpoint route
    catch invalid endpoint route
    
    [fixed $165441966]

--- tests/integration/server.test.js
+++ tests/integration/server.test.js
@@ -2,7 +2,7 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import { startServer } from '../../src/server';
 
-const { expect } = chai;
+const { expect, assert } = chai;
 
 chai.use(chaiHttp);
 
@@ -10,7 +10,7 @@ describe('Root directory Test', () => {
   let app = null;
   let agent = null;
 
-  before(async () => {
+  beforeEach(async () => {
     app = await startServer(5000);
     agent = chai.request(app);
   });
@@ -18,6 +18,14 @@ describe('Root directory Test', () => {
   it('Should return status: 200', (done) => {
     agent.get('/api/v1/').end((_err, res) => {
       expect(res).to.have.status(200);
+      done();
+    });
+  });
+  it('incorrect API endpoint', (done) => {
+    agent.get('/api/v1/incorrect').end((_err, res) => {
+      console.log(res);
+      assert.equal(res.status, 404);
+      assert.equal(res.body.error, 'Incorrect API endpoint');
       done();
     });
   });
