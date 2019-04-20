dfa93aa0280414b41595fef5b2412f78d5554b54
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2019-04-20 05:06:47 -0700

    165465844-bugfix(auth) refactor config (#75)
    -refactor
    
    [Delivers #165465844]

--- src/config/passportConfig.js
+++ src/config/passportConfig.js
@@ -1,31 +1,13 @@
-let CALLBACK_URL_FACEBOOK = `http://${process.env.HOST}:${
-  process.env.PORT
-}/api/v1/auth/facebook/callback`;
-let CALLBACK_URL_TWITTER = `http://${process.env.HOST}:${
-  process.env.PORT
-}/api/v1/auth/twitter/callback`;
-let CALLBACK_URL_GOOGLE = `http://${process.env.HOST}:${
-  process.env.PORT
-}/api/v1/auth/google/callback`;
-
-if (process.env.NODE_ENV === 'production') {
-  CALLBACK_URL_FACEBOOK = `https://${process.env.HOST}:${
-    process.env.PORT
-  }/api/v1/auth/facebook/callback`;
-  CALLBACK_URL_TWITTER = `https://${process.env.HOST}:${
-    process.env.PORT
-  }/api/v1/auth/twitter/callback`;
-  CALLBACK_URL_GOOGLE = `https://${process.env.HOST}:${
-    process.env.PORT
-  }/api/v1/auth/facebook/callback`;
-}
+const CALLBACK_URL_FACEBOOK = `${process.env.BASE_URL}/auth/facebook/callback`;
+const CALLBACK_URL_TWITTER = `${process.env.BASE_URL}/auth/twitter/callback`;
+const CALLBACK_URL_GOOGLE = `${process.env.BASE_URL}/auth/google/callback`;
 
 export default {
   facebookAuth: {
     clientID: process.env.FACEBOOK_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
     callbackURL: CALLBACK_URL_FACEBOOK,
-    profileFields: ['id', 'email', 'displayName', 'photos', 'name'],
+    profileFields: ['id', 'email', 'displayName', 'photos'],
   },
   googleAuth: {
     clientID: process.env.GOOGLE_CLIENT_ID,
