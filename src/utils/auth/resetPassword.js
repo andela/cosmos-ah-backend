ffba8cf30b04c259baea82b031aee672b51f29ed
Martins Aloba <aloba.martins@gmail.com>
2019-04-24 09:45:06 +0100

    164796888 Password Reset Functionality (#71)
    * 164796888-feature(passwordReset): add function to reset password
    
    - add password reset functionality
    - modify the controller
    - add integration test for password reset functionality
    
    [Delivers #164796888]
    
    * 164796888-feature(passwordReset): refactor code
    
    - refactor password reset function to make use of hash method from the users table
    
    [Delivers #164796888]

---
+++ src/utils/auth/resetPassword.js
@@ -0,0 +1,17 @@
+import Validator from 'validatorjs';
+/**
+ * @description This is the method for validating password reset field
+ * @param {object} body The request object
+ * @returns {function} Returns validation object
+ */
+export const validateResetField = (body) => {
+  const rules = {
+    password: ['required', 'confirmed', 'string', 'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\\$%\\^&\\*])(?=.{8,})/i'],
+  };
+  const errorMessages = {
+    'confirmed.password': 'Passwords do not match',
+  };
+  return new Validator(body, rules, errorMessages);
+};
+
+export default validateResetField;
