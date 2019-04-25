d0904f956ac99d2744238b337e5c28b240db83ec
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2019-04-25 18:16:04 +0100

    164796906-feature(auth) Implement like comment
    added like comment utility
    
    [delivers #164796906]

--- src/models/like_comment.js
+++ src/models/like_comment.js
@@ -4,40 +4,40 @@
   * @param {DataTypes} DataTypes
   * @returns {Model} Returns LikeArticle model
   */
- export default (sequelize, DataTypes) => {
-    const LikeComment = sequelize.define(
-      'LikeComment',
-      {
-        id: {
-          type: DataTypes.UUID,
-          defaultValue: DataTypes.UUIDV4,
-          allowNull: false,
-          primaryKey: true,
-        },
-        userId: {
-          type: DataTypes.UUID,
-          allowNull: false,
-        },
-        commentId: {
-          type: DataTypes.UUID,
-          allowNull: false,
-        },
+export default (sequelize, DataTypes) => {
+  const LikeComment = sequelize.define(
+    'LikeComment',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
       },
-      {
-        tableName: 'like_comments',
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
       },
-    );
-    LikeComment.associate = (models) => {
-      LikeComment.belongsTo(models.Comment, {
-        foreignKey: 'commentId',
-        as: 'comment',
-        onDelete: 'CASCADE',
-      });
-      LikeComment.belongsTo(models.User, {
-        foreignKey: 'userId',
-        as: 'user',
-        onDelete: 'CASCADE',
-      });
-    };
-    return LikeComment;
+      commentId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'like_comments',
+    },
+  );
+  LikeComment.associate = (models) => {
+    LikeComment.belongsTo(models.Comment, {
+      foreignKey: 'commentId',
+      as: 'comment',
+      onDelete: 'CASCADE',
+    });
+    LikeComment.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+      onDelete: 'CASCADE',
+    });
   };
+  return LikeComment;
+};
