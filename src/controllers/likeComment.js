691419b436d2890196dec9e83c4a798ac1f721c7
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2019-04-25 18:15:37 +0100

    164796906-feature(auth) Implement like comment
    added like comment utility
    
    [delivers #164796906]

--- src/controllers/likeComment.js
+++ src/controllers/likeComment.js
@@ -1,33 +1,33 @@
-import { likeDislike } from '../utils/likeDislike';
+import { likeDislike } from '../utils/comment';
 import { LikeComment } from '../models';
 import { responseFormat, errorResponseFormat } from '../utils';
 
-  export const likeComment = async (req, res) => {
-   const { id: userId } = req.user;
-   const { id: commentId } = req.params;
-   const condition = { userId, commentId };
-   try {
-     const likeCommentResult = await likeDislike(LikeComment, condition);
-     const { like, likeCount } = likeCommentResult;
-     if (like) {
-       return res.status(200).json(
-         responseFormat({
-           status: 'success',
-           data: { message: 'you successfully liked the comment', Like: likeCount },
-         }),
-       );
-     }
-     return res.status(200).json(
-       responseFormat({
-         status: 'success',
-         data: { message: 'you successfully un-liked the comment', Like: likeCount },
-       }),
-     );
-   } catch (error) {
-     if (error.parent.constraint === 'like_comments_commentId_fkey') {
-       return res
-         .status(400)
-         .json(errorResponseFormat({ message: 'commentId is not on comment Table' }));
-     }
-   }
- };
+export const likeComment = async (req, res) => {
+  const { id: userId } = req.user;
+  const { id: commentId } = req.params;
+  const condition = { userId, commentId };
+  try {
+    const likeCommentResult = await likeDislike(LikeComment, condition);
+    const { like, likeCount } = likeCommentResult;
+    if (like) {
+      return res.status(200).json(
+        responseFormat({
+          status: 'success',
+          data: { message: 'you successfully liked the comment', Like: likeCount },
+        }),
+      );
+    }
+    return res.status(200).json(
+      responseFormat({
+        status: 'success',
+        data: { message: 'you successfully un-liked the comment', Like: likeCount },
+      }),
+    );
+  } catch (error) {
+    if (error.parent.constraint === 'like_comments_commentId_fkey') {
+      return res
+        .status(400)
+        .json(errorResponseFormat({ message: 'commentId is not on comment Table' }));
+    }
+  }
+};
--- src/utils/comment.js
+++ src/utils/comment.js
@@ -17,3 +17,18 @@ export const validateComment = (payload) => {
   };
   return new Validator(payload, rules, errorMessages);
 };
+
+export const likeDislike = async (Model, condition) => {
+  const [, isNewRecord] = await Model.findOrCreate({ where: condition });
+  const { userId, ...countCondition } = condition;
+  if (!isNewRecord) {
+    const deleteRecord = await Model.destroy({ where: condition });
+    if (deleteRecord) {
+      const likeCount = await Model.count({ where: countCondition });
+      return { like: false, likeCount };
+    }
+  } else {
+    const likeCount = await Model.count({ where: countCondition });
+    return { like: true, likeCount };
+  }
+};
