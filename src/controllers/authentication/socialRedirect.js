6af891d31ccfdc87010bf10c68b8cdabd1e70c57
oluwaseyi000 <adebajo.oluwaseyi@gmail.com>
2019-05-07 10:14:07 +0100

    adjust typo error

--- src/controllers/authentication/socialRedirect.js
+++ src/controllers/authentication/socialRedirect.js
@@ -4,7 +4,7 @@ const socialRedirect = (req, res) => {
   const { id, email, role, username } = req.user;
   const token = Authenticator.generateToken({ id, email, role, username });
   // res.redirect(`https://ah-frontend-stage.herokuapp.com?token=${token}`);
-  res.redirect(`http://localhost:9001/token=${token}`);
+  res.redirect(`http://localhost:9001?token=${token}`);
 };
 
 
