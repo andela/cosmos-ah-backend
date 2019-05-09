4388e62d4962efa66c8bdbe04d6899241b52821c
oluwaseyi000 <adebajo.oluwaseyi@gmail.com>
2019-05-09 20:28:54 +0100

    social auth

--- src/controllers/authentication/socialRedirect.js
+++ src/controllers/authentication/socialRedirect.js
@@ -3,8 +3,8 @@ import Authenticator from '../../middlewares/authenticator';
 const socialRedirect = (req, res) => {
   const { id, email, role, username } = req.user;
   const token = Authenticator.generateToken({ id, email, role, username });
-    // res.redirect(`https://ah-frontend-stage.herokuapp.com/handlesocialauth?token=${token}`);
-    res.redirect(`http://localhost:9001/handlesocialauth?token=${token}`);
+    res.redirect(`https://ah-frontend-stage.herokuapp.com/handlesocialauth?token=${token}`);
+    // res.redirect(`http://localhost:9001/handlesocialauth?token=${token}`);
 };
 
 
