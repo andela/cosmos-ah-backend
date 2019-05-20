83939ceee677aab79126f62aa1bef72b3d10e1d7
Micah Akpan <mickey4sure2k7@gmail.com>
2019-05-20 13:23:42 +0100

    166109105-bug(authors): fix author's list bug (#94)
    Return a list of all authors
    instead of a promise
    
    [#166109105]

--- src/controllers/authors.js
+++ src/controllers/authors.js
@@ -3,9 +3,7 @@ import { responseFormat, errorResponseFormat } from '../utils';
 
 const getAuthors = async (req, res) => {
   try {
-    const authors = User.findAll({
-      where: { role: 'author' },
-    });
+    const authors = await User.findAll({ where: { role: 'author' } });
 
     if (authors) {
       return res.status(200).json(responseFormat({ status: 'success', data: authors }));
